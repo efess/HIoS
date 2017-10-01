c32248bf621876d4e652207cd137c2ac0f226273
Joe <efessel@gmail.com>
2017-10-01 17:39:08 -0400

    Fix bug applying empty env vars

--- src/web/server/config.js
+++ src/web/server/config.js
@@ -5,7 +5,7 @@ var extend = require('extend');
 var localConfig = 'local_config.json';
 
 function _applyEnvVar(obj, prop, env) {
-    if(env) {
+    if(typeof env !== "undefined") {
         obj[prop] = env;
     }
 }
@@ -25,7 +25,7 @@ var config = {
             fs.readFile(localConfig, 'utf8', function (err, data) {
                 if (!err) {
                 //     console.log('Coudln\'t load local config file: '  + err);
-                //     reject(err);
+                //     re9ject(err);
                 // } else {
                     var cfgObj = JSON.parse(data);
                     extend(config, cfgObj);  
